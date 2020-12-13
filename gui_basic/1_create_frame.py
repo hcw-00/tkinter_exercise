13f306a9c9fd0e50f33dd358f5c93abd7fa6ffba
ChangWoo HA <changwooha89@gmail.com>
2020-12-13 20:27:59 +0900

    1st_commit

---
+++ gui_basic/1_create_frame.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480") # 가로x세로
+# root.geometry("640x480+100+300") # 가로, 세로, x좌표, y좌표
+
+
+root.resizable(False,False) # 크기 변경 불가
+
+
+root.mainloop()
---
+++ gui_basic/8_combobox.py
@@ -0,0 +1,25 @@
+import tkinter.ttk as ttk
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480") # 가로x세로
+
+values = [str(i)+'일' for i in range(1,32)] 
+combobox = ttk.Combobox(root, height=5, values=values)
+combobox.pack()
+combobox.set("카드 결제일")
+
+readonly_combobox = ttk.Combobox(root, height=10, values=values, state="readonly")
+readonly_combobox.current(0) # 0번째 인덱스 선택
+readonly_combobox.pack()
+
+
+def btncmd():
+    print(combobox.get())
+    print(readonly_combobox.get())
+
+btn = Button(root, text="선택", command=btncmd)
+btn.pack()
+
+root.mainloop()
